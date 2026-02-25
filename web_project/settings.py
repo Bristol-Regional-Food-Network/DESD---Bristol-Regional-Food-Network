e272c12afc0373163010116d4889402a81515dd6
adhamahmeda <adham.aboelmagedd@gmail.com>
2026-02-25 16:59:28 +0000

    Added Producer and Product models, admin registration, migrations, and app setup

--- web_project/settings.py
+++ web_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'producers',
+    'products',
+    'users',
 ]
 
 MIDDLEWARE = [
